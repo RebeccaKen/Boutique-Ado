39355c83e2753d8e61221d87410d46f36d153db9
RebeccaKen <kennedyrebecca694@gmail.com>
2023-05-18 21:05:11 +0000

    Add import env file

--- checkout/views.py
+++ checkout/views.py
@@ -2,16 +2,17 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.conf import settings
 
-
 from .forms import OrderForm
 from bag.contexts import bag_contents
 
 import stripe
-
+import json
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    print(stripe_secret_key)
 
     bag = request.session.get('bag', {})
     if not bag:
@@ -22,10 +23,13 @@ def checkout(request):
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
     stripe.api_key = stripe_secret_key
+    print(stripe.api_key)
     intent = stripe.PaymentIntent.create(
         amount=stripe_total,
         currency=settings.STRIPE_CURRENCY,
     )
+
+    print(intent)
 
     order_form = OrderForm()
 
