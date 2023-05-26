b6f41751bc5888c37365ed0e7a66cece1d95cf29
RebeccaKen <kennedyrebecca694@gmail.com>
2023-05-26 20:17:04 +0000

    Deployment 5 - added tutor changes to setting.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -198,8 +198,8 @@ if 'USE_AWS' in os.environ:
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
-    STATIC_URL = f'https://{boutique-ado-e-commerce-5}.s3.{eu-west-1}.amazonaws.com/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{boutique-ado-e-commerce-5}.s3.{eu-west-1}.amazonaws.com/{MEDIAFILES_LOCATION}/'
+    STATIC_URL = f'https://{boutique-ado-e-commerce-5}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{boutique-ado-e-commerce-5}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
